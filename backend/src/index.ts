1bb7bfa974e18a7affda61be3ef5774fb49e90d3
Ayush Kumar <work.ayced@gmail.com>
2025-08-28 04:36:39 +0530

    db schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,26 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.post("/api/v1/signup", (c) => {});
+app.post("/api/v1/signup", (c) => {
+  return c.text("signup route");
+});
 
-app.post("/api/v1/sigin", (c) => {});
+app.post("/api/v1/sigin", (c) => {
+  return c.text("signin route");
+});
 
-app.post("/api/v1/blog", (c) => {});
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("get blog route");
+});
 
-app.put("/api/v1/blog", (c) => {});
+app.post("/api/v1/blog", (c) => {
+  return c.text("blog post route");
+});
 
-app.get("/api/v1/blog/:id", (c) => {});
+app.put("/api/v1/blog", (c) => {
+  return c.text("blog update route");
+});
 
 export default app;
